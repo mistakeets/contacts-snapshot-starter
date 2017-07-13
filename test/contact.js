5b6e967e87accc664869fdb926b82799021937ca
Keith Oppel <mistakeets@gmail.com>
2017-07-13 13:23:55 -0700

    refactors contact tests to use done(error) method

--- test/contact.js
+++ test/contact.js
@@ -6,7 +6,7 @@ const db = require('./helpers/db')
 
 chai.use(chaiHttp)
 
-describe.skip('/contacts', () => {
+describe('/contacts', () => {
 
   beforeEach(() => {
     return db.initDb()
@@ -18,35 +18,26 @@ describe.skip('/contacts', () => {
       .type('form')
       .send({ first_name: 'That', last_name: 'Person' })
       .end((error, response) => {
-        if (error) {
-          done(error)
-        }
         expect(response).to.have.status(200)
+        done(error)
       })
-    done()
   })
 
   it('should delete a contact', (done) => {
     chai.request('http://localhost:3000')
       .get('/contacts/2/delete')
       .end((error, response) => {
-        if (error) {
-          done(error)
-        }
         expect(response.text).to.not.contain('<h1>Tanner&nbsp;Welsh</h1>')
+        done(error)
       })
-    done()
   })
 
   it('should return a first name and last name', (done) => {
     chai.request('http://localhost:3000')
       .get('/contacts/1')
       .end((error, response) => {
-        if (error) {
-          done(error)
-        }
         expect(response.text).to.contain('<h1>Jared&nbsp;Grippe</h1>')
+        done(error)
       })
-    done()
   })
 })
