4e69613e4054cf767230d6b397db57561045dcf8
Keith Oppel <mistakeets@gmail.com>
2017-07-31 18:33:03 -0700

    WIP: refactoring project to cleanup codebase and add auth

--- src/routes/contacts.js
+++ src/routes/contacts.js
@@ -1,52 +1,51 @@
-const express = require('express')
-const database = require('../../database')
-const {renderError} = require('../utils')
+const DbContacts = require('../database/contacts')
+const { renderError } = require('../server/utils')
 
-const router = express.Router()
+const router = require('express').Router()
 
 router.get('/new', (request, response) => {
   response.render('new')
 })
 
-router.post('/', (request, response) => {
-  database.createContact(request.body)
+router.post('/', (request, response, next) => {
+  DbContacts.createContact(request.body)
     .then(function(contact) {
       if (contact) return response.redirect(`/contacts/${contact[0].id}`)
       next()
     })
-    .catch( error => renderError(error, response, response) )
+    .catch(error => renderError(error, response, response))
 })
 
 router.get('/:contactId', (request, response, next) => {
   const contactId = request.params.contactId
   if (!contactId || !/^\d+$/.test(contactId)) return next()
-  database.getContact(contactId)
+  DbContacts.getContact(contactId)
     .then(function(contact) {
       if (contact) return response.render('show', { contact })
       next()
     })
-    .catch( error => renderError(error, response, response) )
+    .catch(error => renderError(error, response, response))
 })
 
 
-router.get('/:contactId/delete', (request, response) => {
+router.get('/:contactId/delete', (request, response, next) => {
   const contactId = request.params.contactId
-  database.deleteContact(contactId)
+  DbContacts.deleteContact(contactId)
     .then(function(contact) {
       if (contact) return response.redirect('/')
       next()
     })
-    .catch( error => renderError(error, response, response) )
+    .catch(error => renderError(error, response, response))
 })
 
-router.get('/search', (request, response) => {
+router.get('/search', (request, response, next) => {
   const query = request.query.q
-  database.searchForContact(query)
+  DbContacts.searchForContact(query)
     .then(function(contacts) {
       if (contacts) return response.render('index', { query, contacts })
       next()
     })
-    .catch( error => renderError(error, response, response) )
+    .catch(error => renderError(error, response, response))
 })
 
 module.exports = router
---
+++ src/routes/index.js
@@ -0,0 +1,51 @@
+const router = require('express').Router();
+const contacts = require('./contacts')
+const DbContacts = require('../database/contacts');
+const DbUsers = require('../database/users')
+const bcrypt = require('bcrypt')
+const passport = require('../../config/auth')
+
+router.get('/signup', (request, response) => {
+  response.render('signup')
+})
+
+router.post('/signup', (request, response, next) => {
+  const salt = bcrypt.genSaltSync(10)
+  const hash = bcrypt.hashSync(request.body.password, salt)
+
+  const credentials = {
+    'name': request.body.name,
+    'email': request.body.email,
+    'password': hash
+  }
+
+  DbUsers.createUser(credentials)
+    .then(function(contact) {
+      if (contact)
+        return response.redirect(`/login`)
+      next()
+    })
+    .catch(error => renderError(error, response, response))
+})
+
+router.get('/login', (request, response) => {
+  response.render('login')
+})
+
+router.post('/login',
+  passport.authenticate('local', {
+    successRedirect: '/',
+    successFailure: '/login'
+  }))
+
+
+router.get('/', (request, response) => {
+  DbContacts.getContacts()
+    .then((contacts) => { response.render('index', { contacts }) })
+    .catch(err => console.log('err', err))
+})
+
+router.use('/contacts', contacts); // /contacts/search
+// router.use('/auth', auth) // /auth/signup ...
+
+module.exports = router;
